50738c0b3b58b5441dfb7117f752d5e7ee63efe9
ShreypatelA5 <102640909+ShreypatelA5@users.noreply.github.com>
2023-01-29 03:37:06 -0500

    commit chnage

--- server.js
+++ server.js
@@ -9,14 +9,11 @@ function onHttpStart() {
 }
 
 // setup a 'route' to listen on the default url path (http://localhost)
-app.get("/", function(req,res){
-    res.send("Hello World<br /><a href='/about'>Go to the about page</a>");
+app.get("/about", function(req,res){
+    res.send("/views/about.html");
 });
 
-// setup another route to listen on /about
-app.get("/about", function(req,res){
-    res.send("<h3>About</h3>");
-});
+
 
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
