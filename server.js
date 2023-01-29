733b698571fa47e23e499d887518d3cf2b39f75b
ShreypatelA5 <102640909+ShreypatelA5@users.noreply.github.com>
2023-01-29 12:23:23 -0500

    c

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
 }
 
+app.use(express.static("public"));
 // setup a 'route' to listen on the default url path (http://localhost)
-app.get("/", function(res){
-  res.redirect('/views/about.html');
+app.get("/", function(){
+  //res.redirect('/views/about.html');
 });
 
 // setup another route to listen on /about
 app.get("/about", function(req,res){
-    res.sendFile(path.join(__dirname, "/views/about.html"));
+  res.redirect('/views/about.html');
 });
 
 // setup http server to listen on HTTP_PORT
