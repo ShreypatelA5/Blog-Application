4d8d7e0c53c66a5fabe58d58201a693ed8775f6a
ShreypatelA5 <102640909+ShreypatelA5@users.noreply.github.com>
2023-01-29 11:18:39 -0500

    c

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-
-var HTTP_PORT = process.env.PORT || 8080;
-
-// call this function after the http server starts listening for requests
-function onHttpStart() {
-  console.log("Express http server listening on: " + HTTP_PORT);
-}
-app.use(express.static('views'));
-// setup a 'route' to listen on the default url path (http://localhost)
-app.get("/", function(req,res){
-    res.redirect('/views/about.html');
+var PORT = 3000;
+  
+// Without middleware
+app.get('/', function(req, res){
+    res.redirect('/about');
 });
-
-// setup another route to listen on /about
-app.get("/", function(req,res){
-    res.send("<h3>About</h3>");
+  
+app.get('/about', function(req, res){
+    res.send("Redirected to User Page");
 });
-
-// setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT, onHttpStart);
+  
+app.listen(PORT, function(err){
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
