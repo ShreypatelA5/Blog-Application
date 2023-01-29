9974c8a2e752482c7ddc431562bf03af2f080486
ShreypatelA5 <102640909+ShreypatelA5@users.noreply.github.com>
2023-01-29 03:35:53 -0500

    initial commit

--- server.js
+++ server.js
@@ -8,8 +8,15 @@ function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
 
-res.redirect("/about", function(req,res){
-    res.send("/views/about.html");
+// setup a 'route' to listen on the default url path (http://localhost)
+app.get("/", function(req,res){
+    res.send("Hello World<br /><a href='/about'>Go to the about page</a>");
 });
 
+// setup another route to listen on /about
+app.get("/about", function(req,res){
+    res.send("<h3>About</h3>");
+});
+
+// setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
