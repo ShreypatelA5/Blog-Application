5c080c66885ff9bf40731e46f91dbf0dc03819c6
ShreypatelA5 <102640909+ShreypatelA5@users.noreply.github.com>
2023-01-29 03:59:13 -0500

    change

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
 
+app.use(express.static('about.html'));
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/about",function(res){
     res.redirect('/views/about.html');
