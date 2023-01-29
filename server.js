68e91c4628570f0aa21441c6e7246d0544ecf3d6
ShreypatelA5 <102640909+ShreypatelA5@users.noreply.github.com>
2023-01-29 10:45:40 -0500

    commit

--- server.js
+++ server.js
@@ -1,36 +1,22 @@
-const express = require("express");
-const app = express();
+var express = require("express");
+var app = express();
 
-const HTTP_PORT = process.env.PORT || 8080;
+var HTTP_PORT = process.env.PORT || 8080;
 
 // call this function after the http server starts listening for requests
 function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
 
-// setup a route on the 'root' of the url
-// IE: http://localhost:8080/
-app.get("/", (req, res) => {
-  res.send("/views/about.html");
+// setup a 'route' to listen on the default url path (http://localhost)
+app.get("/", function(req,res){
+    res.send("Hello World<br /><a href='/about'>Go to the about page</a>");
 });
 
-// now add a route for the /headers page
-// IE: http://localhost:8080/headers
-app.get("/about", (req, res) => {
-  const headers = req.headers;
-  res.send(/views/about.html);
+// setup another route to listen on /about
+app.get("/about", function(req,res){
+    res.send("<h3>About</h3>");
 });
 
-// This use() will not allow requests to go beyond it
-// so we place it at the end of the file, after the other routes.
-// This function will catch all other requests that don't match
-// any other route handlers declared before it.
-// This means we can use it as a sort of 'catch all' when no route match is found.
-// We use this function to handle 404 requests to pages that are not found.
-app.use((req, res) => {
-  res.status(404).send("Page Not Found");
-});
-
-// listen on port 8080\. The default port for http is 80, https is 443\. We use 8080 here
-// because sometimes port 80 is in use by other applications on the machine
+// setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
