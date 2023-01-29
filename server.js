899689473a85e4f4b776640c93f52d32d4be7aff
ShreypatelA5 <102640909+ShreypatelA5@users.noreply.github.com>
2023-01-29 11:22:00 -0500

    c

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
-  
+app.use(express.static('views'));
 // Without middleware
 app.get('/', function(req, res){
     res.redirect('/about');
 });
   
 app.get('/about', function(req, res){
-    res.send("Redirected to User Page");
+    res.redirect('/views/about.html');
 });
   
 app.listen(PORT, function(err){
