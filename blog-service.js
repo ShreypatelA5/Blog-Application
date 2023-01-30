e05c1868b837237c1cab34aecddf9b3f14efba44
ShreypatelA5 <102640909+ShreypatelA5@users.noreply.github.com>
2023-01-29 23:39:42 -0500

    c

--- blog-service.js
+++ blog-service.js
@@ -1,12 +1,18 @@
 const fs = require('fs');
 
 const getPublishedPosts = () => {
-const rawData = fs.readFileSync('/data/posts.json');
+const filePath = './data/posts.json';
+if (fs.existsSync(filePath)) {
+const rawData = fs.readFileSync(filePath);
 const posts = JSON.parse(rawData);
 const publishedPosts = posts.filter(post => post.published === true);
 return publishedPosts;
+} else {
+console.error(`Error: The file ${filePath} could not be found`);
+}
 };
 
 module.exports = {
 getPublishedPosts,
 };
+
