15dedd374fd70e4079d8bc05ec805c8759334000
ShreypatelA5 <102640909+ShreypatelA5@users.noreply.github.com>
2023-01-29 23:16:32 -0500

    c

--- server.js
+++ server.js
@@ -20,14 +20,13 @@ app.get("/about", function (req, res) {
   res.sendFile(__dirname + "/views/about.html");
 });
 
-app.get('/blog', (req, res) => {
-  res.send('TODO: get all posts who have published==true');
-});
+//app.get('/blog', (req, res) => {
+ // res.send('TODO: get all posts who have published==true');
+//});
 
 app.get('/blog', (req, res) => {
   const posts = blogService.getPublishedPosts();
   res.json(posts);
 });
-
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
