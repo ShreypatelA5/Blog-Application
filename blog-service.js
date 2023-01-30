acc1fc7db2490ba35ae5a4b9cc593292ff5632b7
ShreypatelA5 <102640909+ShreypatelA5@users.noreply.github.com>
2023-01-30 02:10:46 -0500

    c

--- blog-service.js
+++ blog-service.js
@@ -4,28 +4,3 @@ const fs = require('fs');
 const getPublishedPosts = () => {
 const filePath = './data/posts.json';
 if (fs.existsSync(filePath)) {
-const rawData = fs.readFileSync(filePath);
-const posts = JSON.parse(rawData);
-const publishedPosts = posts.filter(post => post.published === true);
-return publishedPosts;
-} else {
-console.error(`Error: The file ${filePath} could not be found`);
-}
-};
-
-//Return JSON format to POSTS
-function getAllPosts() {
-  return JSON.parse(fs.readFileSync('./data/posts.json'));
-}
-
-function getAllCategories() {
-    return JSON.parse(fs.readFileSync('./data/categories.json'));
-}
-
-module.exports = {
-    getPublishedPosts,
-    getAllPosts,
-    getAllCategories
-};
-
-
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var path = require("path");
 const blogService = require('./blog-service');
+const fs = require('fs');
 
 var HTTP_PORT = process.env.PORT || 8080;
 
@@ -20,15 +21,37 @@ app.get("/about", function (req, res) {
   res.sendFile(__dirname + "/views/about.html");
 });
 
+const getPublishedPosts = () => {
+  const filePath = './data/posts.json';
+  if (fs.existsSync(filePath)) {
+  const rawData = fs.readFileSync(filePath);
+  const posts = JSON.parse(rawData);
+  const publishedPosts = posts.filter(post => post.published === true);
+  return publishedPosts;
+  } else {
+  console.error(`Error: The file ${filePath} could not be found`);
+  }
+  };
+
 app.get('/blog', (req, res) => {
   const publishedPosts = blogService.getPublishedPosts();
   res.json(publishedPosts);
   });
 
+  //Return JSON format to POSTS
+function getAllPosts() {
+  return JSON.parse(fs.readFileSync('./data/posts.json'));
+}
+
+
   app.get('/posts', (req, res) => {
     const posts = blogService.getAllPosts();
     res.json(posts);
   });
+
+  function getAllCategories() {
+    return JSON.parse(fs.readFileSync('./data/categories.json'));
+}
 
   app.get('/categories', (req, res) => {
     const posts = blogService.getAllCategories();
@@ -39,5 +62,13 @@ app.get('/blog', (req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.jpeg'));
   });
   
+  module.exports = {
+    getPublishedPosts,
+    getAllPosts,
+    getAllCategories
+};
+
+
+
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
