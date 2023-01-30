dbbc123be35e091d2253baf7448501f398b5a707
ShreypatelA5 <102640909+ShreypatelA5@users.noreply.github.com>
2023-01-30 01:29:59 -0500

    c

--- blog-service.js
+++ blog-service.js
@@ -13,15 +13,18 @@ console.error(`Error: The file ${filePath} could not be found`);
 }
 };
 
-
-
 //Return JSON format to POSTS
 function getAllPosts() {
   return JSON.parse(fs.readFileSync('./data/posts.json'));
 }
 
+function getAllCategories() {
+    return JSON.parse(fs.readFileSync('./data/categories.json'));
+}
+
 module.exports = {
     getPublishedPosts,
-    getAllPosts
+    getAllPosts,
+    getAllCategories
 };
 
