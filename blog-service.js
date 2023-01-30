de8a9f301eadfdfca9a6d9c0b149964e3c5d69c3
ShreypatelA5 <102640909+ShreypatelA5@users.noreply.github.com>
2023-01-29 23:25:18 -0500

    c

--- blog-service.js
+++ blog-service.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const getPublishedPosts = () => {
-const rawData = fs.readFileSync('./posts.json');
+const rawData = fs.readFileSync('.data//posts.json');
 const posts = JSON.parse(rawData);
 const publishedPosts = posts.filter(post => post.published === true);
 return publishedPosts;
