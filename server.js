11e2319d19013522a77333f31994d0b0996b212b
ShreypatelA5 <102640909+ShreypatelA5@users.noreply.github.com>
2023-02-01 20:25:50 -0500

    commit

--- server.js
+++ server.js
@@ -26,37 +26,40 @@ function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
 }
 
-app.use(express.static("public"));
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function (req, res) {
   res.redirect("/about");
 });
 
+// this function redirect the user to the about page
 app.get("/about", function (req, res) {
   res.sendFile(__dirname + "/views/about.html");
 });
 
+//This function redirect the user to the blog page
 app.get('/blog', (req, res) => {
   const publishedPosts = blogService.getPublishedPosts();
   res.json(publishedPosts);
   });
 
+  //This function redirect the user to the posts page
   app.get('/posts', (req, res) => {
     const posts = blogService.getAllPosts();
     res.json(posts);
   });
 
+  //This function redirect the user to the categories page
   app.get('/categories', (req, res) => {
     const posts = blogService.getAllCategories();
     res.json(posts);
   });
 
+  // This function called when no any matching route found in URL
   app.use((req, res, next) => {
     res.status(404).sendFile(__dirname + "/views/404-2.webp");
   });
   
 
-  
 blogService.initialize()
 .then(() => {
   app.listen(HTTP_PORT, () => {
@@ -65,6 +68,27 @@ blogService.initialize()
 })
 .catch(err => {
   console.error("Unable to start the server:", err);
+});
+
+
+app.get('/blog', (req, res) => {
+  blogService.getCategories()
+    .then((data) => {
+      res.json({blogs: data});
+    })
+    .catch((err) => {
+      res.status(500).json({message: err});
+    });
+});
+
+app.get('/posts', (req, res) => {
+  blogService.getCategories()
+    .then((data) => {
+      res.json({posts: data});
+    })
+    .catch((err) => {
+      res.status(500).json({message: err});
+    });
 });
 
 app.get('/categories', (req, res) => {
