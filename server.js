aae9d415d64d19709880b14ca3e95840186a99f6
ShreypatelA5 <102640909+ShreypatelA5@users.noreply.github.com>
2023-02-01 20:38:13 -0500

    Update

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ var HTTP_PORT = process.env.PORT || 8080;
 function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
 }
+
 app.use(express.static("public"));
+
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function (req, res) {
   res.redirect("/about");
