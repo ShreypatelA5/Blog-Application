4eecb9300eaf80c81d58902321aa6ee46f5d327d
ShreypatelA5 <102640909+ShreypatelA5@users.noreply.github.com>
2023-02-01 20:33:32 -0500

    Update server.js

--- server.js
+++ server.js
@@ -26,45 +26,77 @@ function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
 }
 
-app.use(express.static("public"));
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function (req, res) {
   res.redirect("/about");
 });
 
+// this function redirect the user to the about page
 app.get("/about", function (req, res) {
   res.sendFile(__dirname + "/views/about.html");
 });
 
+//This function redirect the user to the blog page
 app.get('/blog', (req, res) => {
   const publishedPosts = blogService.getPublishedPosts();
   res.json(publishedPosts);
   });
 
+  //This function redirect the user to the posts page
   app.get('/posts', (req, res) => {
     const posts = blogService.getAllPosts();
     res.json(posts);
   });
 
+  //This function redirect the user to the categories page
   app.get('/categories', (req, res) => {
     const posts = blogService.getAllCategories();
     res.json(posts);
   });
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(__dirname + "/views/vecteezy_404-landing-page_6549647.jpg");
+  // This function called when no any matching route found in URL
+  app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + "/views/404-2.webp");
   });
   
+
+//Initialize the blog service 
 blogService.initialize()
 .then(() => {
+  // Start the server if the initialize() method is successful
   app.listen(HTTP_PORT, () => {
     console.log("Server started on port" + HTTP_PORT);
   });
 })
 .catch(err => {
+  // Output an error message if the initialize() method returns a
   console.error("Unable to start the server:", err);
 });
 
+
+// Make call to the service and fetch data to be returned to the client
+app.get('/blog', (req, res) => {
+  blogService.getCategories()
+    .then((data) => {
+      res.json({blogs: data});
+    })
+    .catch((err) => {
+      res.status(500).json({message: err});
+    });
+});
+
+// Make call to the service and fetch data to be returned to the client
+app.get('/posts', (req, res) => {
+  blogService.getCategories()
+    .then((data) => {
+      res.json({posts: data});
+    })
+    .catch((err) => {
+      res.status(500).json({message: err});
+    });
+});
+
+// Make call to the service and fetch data to be returned to the client
 app.get('/categories', (req, res) => {
   blogService.getCategories()
     .then((data) => {
@@ -76,5 +108,3 @@ app.get('/categories', (req, res) => {
 });
 
 
-// setup http server to listen on HTTP_PORT
-//app.listen(HTTP_PORT, onHttpStart);
